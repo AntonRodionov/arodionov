a119d475231c4895744fd6429a7994dca36b2183
Anton Rodionov <fswl@bk.ru>
2017-05-08 14:03:40 +0200

    1.3 javadoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,12 @@
 package ru.job4j;
+/**
+Class Calculate чаптер_001
+@author arodionov
+@since 08.05.2017
+*/
 
 public class Calculate {
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
-	
-	
 }

974e2ed9eb0bd1ea5bd25546d90d4312bb71fb6b
Anton Rodionov <fswl@bk.ru>
2017-05-08 17:37:47 +0200

    1.5 checkstyle done

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,16 @@
 package ru.job4j;
 /**
-Class Calculate чаптер_001
-@author arodionov
-@since 08.05.2017
+* Class Calculate чаптер_001.
+* @author arodionov
+* @since 08.05.2017
 */
 
 public class Calculate {
 /**
-Main.
-@param args - args.
+*Main.
+*@param args - args.
 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }

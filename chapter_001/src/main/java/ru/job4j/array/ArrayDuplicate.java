a931d63ad79c7b9448359ad3eb637d35555b09c6
Anton Rodionov <fswl@bk.ru>
2017-05-13 19:33:04 +0200

    5.3 v.2

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -20,13 +20,14 @@ public class ArrayDuplicate {
         int n = 0;
         String s;
         while (i < razmer - k) {
-            for (int j = 0; j < razmer - k; j++) {
+            for (int j = i + 1; j < razmer - k; j++) {
                 if (i != j) {
                     if (array[i] == array[j]) {
                         s = array[j];
                         array[j] = array[razmer - 1 - k];
                         array[razmer - 1 - k] = s;
                         k++;
+						j--;
                     }
                 }
             }
@@ -35,6 +36,6 @@ public class ArrayDuplicate {
             }
             n = k;
         }
-		return Arrays.copyOf(array, razmer - 2);
+		return Arrays.copyOf(array, razmer - k);
 	}
 }
